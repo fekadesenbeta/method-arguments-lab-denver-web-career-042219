9afd4f627bbf63b41afe64fe41f46df8289b4ba5
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-17 18:51:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@ def introduction(name)
   puts "Hi, my name is {name}."
 end
 
-def introduction(name, "Language")
-  puts "Hi, my name is {name, "Language"}."
+def introduction(name, Language)
+  puts "Hi, my name is {name, Language}."
 end
 
